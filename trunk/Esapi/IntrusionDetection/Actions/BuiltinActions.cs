8d79ce312f6a0a76728b4818a143eba3b3a7b7b1
apbogdan <apbogdan@7b01de52-4441-0410-8ea3-0525507bbad3>
2009-09-25 02:43:15 +0000

    Changes: - instrusion detector GetAction - actions : block, redirect and transfer - changed builtin action names

--- trunk/Esapi/IntrusionDetection/Actions/BuiltinActions.cs
+++ trunk/Esapi/IntrusionDetection/Actions/BuiltinActions.cs
@@ -9,16 +9,31 @@ namespace Owasp.Esapi.IntrusionDetection.Actions
         /// <summary>
         /// Log action
         /// </summary>
-        public const string Log = "BuiltinActions.Log";
+        public const string Log = "Log";
 
         /// <summary>
         /// Disable ASP.NET Membership account
         /// </summary>
-        public const string MembershipDisable = "BuiltinActions.MembershipDisable";
+        public const string MembershipDisable = "MembershipDisable";
 
         /// <summary>
         /// Do Forms authentication logout
         /// </summary>
-        public const string FormsAuthenticationLogout = "BuiltinActions.FormsAuthenticationLogout";
+        public const string FormsAuthenticationLogout = "FormsAuthenticationLogout";
+
+        /// <summary>
+        /// Block HTTP request
+        /// </summary>
+        public const string Block = "Block";
+
+        /// <summary>
+        /// Redirect user to page
+        /// </summary>
+        public const string Redirect = "Redirect";
+
+        /// <summary>
+        /// Transfer user to page
+        /// </summary>
+        public const string Transfer = "Transfer";
     }
 }
