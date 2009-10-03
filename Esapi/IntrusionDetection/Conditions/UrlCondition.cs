00b795ff7c8a6a67445b8f0b7c7c7f1afc361655
apbogdan <apbogdan@7b01de52-4441-0410-8ea3-0525507bbad3>
2009-10-03 03:08:38 +0000

    Initial check in for intrusion detection rule engine : rules and conditions definition

---
+++ Esapi/IntrusionDetection/Conditions/UrlCondition.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Owasp.Esapi.IntrusionDetection.Conditions
+{
+    /// <summary>
+    /// Regex based URL context condition
+    /// </summary>
+    public class UrlCondition : IContextCondition
+    {
+        /// <summary>
+        /// Any URL pattern
+        /// </summary>
+        private const string AnyUrlPattern = "*";
+
+        private Regex _url;
+
+        /// <summary>
+        /// Intialize URL condition
+        /// </summary>
+        /// <param name="urlPattern">URL Pattern</param>
+        public UrlCondition(string urlPattern)
+        {
+            UrlPattern = urlPattern;
+        }
+
+        /// <summary>
+        /// URL pattern
+        /// </summary>
+        public string UrlPattern
+        {
+            get { return _url.ToString(); }
+            set
+            {
+                if (string.IsNullOrEmpty(value)) {
+                    _url = new Regex("^$");
+                }
+                else {
+                    _url = new Regex(value);
+                }
+            }
+                
+        }
+
+        #region IContextSelector Members
+
+        /// <summary>
+        /// Verify URL condition
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public bool Evaluate(ContextConditionArgs args)
+        {
+            if (args == null) {
+                throw new ArgumentNullException();
+            }
+
+            return _url.IsMatch(args.RequestUri.ToString());
+        }
+
+        #endregion
+    }
+}
---
+++ Esapi/IntrusionDetection/IntrusionRuleArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+using Owasp.Esapi.Interfaces;
+
+namespace Owasp.Esapi.IntrusionDetection
+{
+    /// <summary>
+    /// Input rule arguments
+    /// </summary>
+    public class IntrusionInputRuleArgs 
+    {        
+    }
+
+    /// <summary>
+    /// Output rule arguments
+    /// </summary>
+    public class IntrusionOutputRuleArgs
+    {
+    }
+}
