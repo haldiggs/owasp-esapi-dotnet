bf5c6b5ae0f6cd4261d3f3f4713baabd42d4d0ad
apbogdan <apbogdan@7b01de52-4441-0410-8ea3-0525507bbad3>
2009-10-15 01:57:45 +0000

    Changes summary: - refactor add-ons instance configuration (prepare the code for object builder) - fixed swingset

--- Esapi/Configuration/ActionElements.cs
+++ Esapi/Configuration/ActionElements.cs
@@ -139,57 +139,7 @@ namespace Owasp.Esapi.Configuration
     /// <summary>
     /// The ActionElement Configuration Element.
     /// </summary>
-    public class ActionElement : ConfigurationElement
+    public class ActionElement : ObjectInstanceElement
     {
-        #region Type Property
-
-        /// <summary>
-        /// The XML name of the <see cref="Type"/> property.
-        /// </summary>
-        internal const string TypePropertyName = "type";
-
-        /// <summary>
-        /// Gets or sets the Type.
-        /// </summary>
-        [ConfigurationProperty(TypePropertyName, IsRequired = false, IsKey = false, IsDefaultCollection = false)]
-        public string Type
-        {
-            get
-            {
-                return (string)base[TypePropertyName];
-            }
-            set
-            {
-                base[TypePropertyName] = value;
-            }
-        }
-
-        #endregion
-
-        #region Name Property
-
-        /// <summary>
-        /// The XML name of the <see cref="Name"/> property.
-        /// </summary>
-        internal const string NamePropertyName = "name";
-
-        /// <summary>
-        /// Gets or sets the Name.
-        /// </summary>
-        [ConfigurationProperty(NamePropertyName, IsRequired = true, IsKey = true, IsDefaultCollection = false)]
-        public string Name
-        {
-            get
-            {
-                return (string)base[NamePropertyName];
-            }
-            set
-            {
-                base[NamePropertyName] = value;
-            }
-        }
-
-        #endregion
-
     }
 }
--- Esapi/Configuration/ConditionElements.cs
+++ Esapi/Configuration/ConditionElements.cs
@@ -5,83 +5,8 @@ namespace Owasp.Esapi.Configuration
     /// <summary>
     /// The ConditionElement Configuration Element.
     /// </summary>
-    public partial class ConditionElement : ConfigurationElement
+    public partial class ConditionElement : ObjectInstanceElement
     {
-        #region Name Property
-
-        /// <summary>
-        /// The XML name of the <see cref="Name"/> property.
-        /// </summary>
-        internal const string NamePropertyName = "name";
-
-        /// <summary>
-        /// Gets or sets the Name.
-        /// </summary>
-        [ConfigurationProperty(NamePropertyName, IsRequired = true, IsKey = true, IsDefaultCollection = false)]
-        public string Name
-        {
-            get
-            {
-                return (string)base[NamePropertyName];
-            }
-            set
-            {
-                base[NamePropertyName] = value;
-            }
-        }
-
-        #endregion
-
-        #region Type Property
-
-        /// <summary>
-        /// The XML name of the <see cref="Type"/> property.
-        /// </summary>
-        internal const string TypePropertyName = "type";
-
-        /// <summary>
-        /// Gets or sets the Type.
-        /// </summary>
-        [ConfigurationProperty(TypePropertyName, IsRequired = true, IsKey = false, IsDefaultCollection = false)]
-        public string Type
-        {
-            get
-            {
-                return (string)base[TypePropertyName];
-            }
-            set
-            {
-                base[TypePropertyName] = value;
-            }
-        }
-
-        #endregion
-
-        #region properties Property
-
-        /// <summary>
-        /// The XML name of the <see cref="properties"/> property.
-        /// </summary>
-        internal const string PropertiesPropertyName = "properties";
-
-        /// <summary>
-        /// Gets or sets the properties.
-        /// </summary>
-        [ConfigurationProperty(PropertiesPropertyName, IsRequired = false, IsKey = false, IsDefaultCollection = false)]
-        public PropertyCollection Properties
-        {
-            get
-            {
-                return (PropertyCollection)base[PropertiesPropertyName];
-            }
-            set
-            {
-                base[PropertiesPropertyName] = value;
-            }
-        }
-
-        #endregion
-
     }
 
     /// <summary>
